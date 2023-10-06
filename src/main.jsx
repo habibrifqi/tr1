c92525b69c68854e8736a8696332c016cb81bbb3
habibrifqi <48439343+habibrifqi@users.noreply.github.com>
2023-10-07 06:52:42 +0700

    perbaikan css 2

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './dist/css/main.css';
+import './dist/css/main.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
